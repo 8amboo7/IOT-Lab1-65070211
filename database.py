a1cfb4b0829424aa83f73ee1af2114b0e2ae7948
SarutaBamboo <142737838+8amboo7@users.noreply.github.com>
2024-07-31 09:46:43 +0700

    Update .gitignore and database.py, fix requirements.txt

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///./sql.db')
 
-DATABASE_URL = 'sqlite:///./sql.db'
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if 'sqlite' in SQLALCHEMY_DATABASE_URL else {}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
